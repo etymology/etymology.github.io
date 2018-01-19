8a1fb62bed639b16a20b38087806d88bf7457f2d
etymology <benjamin.oye@gmail.com>
2018-01-19 11:03:21 -0600

    minimal mistakes tweaks

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "Exposure",
+        "excerpt":"The first post. It has some text.And a line break now.","categories": [],
+        "tags": ["personal"],
+        "url": "http://localhost:4000/exposure/",
+        "teaser":null}]
